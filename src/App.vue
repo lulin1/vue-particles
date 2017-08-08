f6d3433a4b607f9f701616fb8be5f2344587c403
1 <lulin1@xiaomi.com>
2017-08-08 15:27:42 +0800

    Init pro and add particle effect

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <vue-particles 
+      color="#dedede"
+      :particleOpacity="0.8"
+      :particlesNumber="100"
+      shapeType="star"
+      :particleSize="4"
+      linesColor="#dedede"
+      :linesWidth="1"
+      :lineLinked="true"
+      :lineOpacity="0.4"
+      :linesDistance="150"
+      :moveSpeed="3"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+    >
+    </vue-particles>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+body {
+  background: aliceblue;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
